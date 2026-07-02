90450803e3d0903eb15b2a0e9358a5b6c162736c
kaungmyat999 <kaungmyat30519991@gmail.com>
2026-07-02 04:46:43 -0400

    added vercel analystics

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 
 import App from './app/app';
+
+inject();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
